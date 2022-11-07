284a670c3c7355cfb28e1f382627bbc246013907
chase <c@chse.dev>
2022-11-06 21:05:31 -0600

    switch link & link name on /edit

--- public/js/edit.js
+++ public/js/edit.js
@@ -72,16 +72,16 @@ function addLink(index, link, linkName)
         hrForMobile.id = `hrForMobile`;
         linkDiv.append(hrForMobile);
     }
-    // linkDiv.innerHTML += `<label for="link${ index }">Link ${ index + 1 }</label>`;
-    linkDiv.innerHTML += `<input type="text" id="link${ index }" name="link${ index }" value="${ link }" placeholder="Link" required>`;
+    // linkDiv.innerHTML += `<label for="linkName${ index }">Link Name ${ index + 1 }</label>`;
+    linkDiv.innerHTML += linkName !== `` ? `<input type="text" id="linkName${ index }" name="linkName${ index }" value="${ linksNamesArray[index] }" placeholder="Link Name" required>` : `<input type="text" id="linkName${ index }" name="linkName${ index }" placeholder="Link Name" required>`;
 
     const brForLinkName = document.createElement(`br`);
     brForLinkName.id = `pageBreakForButtons`;
     brForLinkName.style = `line-height: 12em;`;
     linkDiv.append(brForLinkName);
 
-    // linkDiv.innerHTML += `<label for="linkName${ index }">Link Name ${ index + 1 }</label>`;
-    linkDiv.innerHTML += linkName !== `` ? `<input type="text" id="linkName${ index }" name="linkName${ index }" value="${ linksNamesArray[index] }" placeholder="Link Name" required>` : `<input type="text" id="linkName${ index }" name="linkName${ index }" placeholder="Link Name" required>`;
+    // linkDiv.innerHTML += `<label for="link${ index }">Link ${ index + 1 }</label>`;
+    linkDiv.innerHTML += `<input type="text" id="link${ index }" name="link${ index }" value="${ link }" placeholder="Link" required>`;
 
     const brForButtons = document.createElement(`br`);
     brForButtons.id = `pageBreakForButtons`;
